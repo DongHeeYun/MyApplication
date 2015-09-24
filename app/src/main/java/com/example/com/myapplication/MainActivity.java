711782696d8fcd470f558656077ce75ecfd10feb
DongHeeYun <inter0960@naver.com>
2015-09-24 10:28:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.com.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"@string/pressBtn1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
